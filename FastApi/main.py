53ccd3bfa0dbd57ca37904e9f350c53ae6deb6fa
Juan David Cordoba Ortega <juan941901@gmail.com>
2025-10-21 21:53:10 -0500

    agregamos la parte de contenido estatico

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import users, products
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -7,6 +8,10 @@ app = FastAPI()
 app.include_router(users.router)
 app.include_router(products.router)
 
+# Montar archivos estáticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Endpoint raíz
 @app.get("/")
 async def root():
     return {"message" : "¡Hola FastApi!" }
